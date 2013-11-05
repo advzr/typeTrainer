38e4cdc422bbb72171f8724a613167287aa15a34
Dmitry Gavrikov <dmitry923@gmail.com>
2013-11-05 17:08:32 +0700

    Initial file structure

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+require.config({
+  paths: {
+    /* external libs */
+    'jquery': '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min',
+    /* local paths */
+    'template': '../template'
+  }
+});
+
+require([], function() {
+});
